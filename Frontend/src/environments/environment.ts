e4bae3fcc0d4051f12d231830bab9477409b2eeb
eran samimian <69861689+eransam@users.noreply.github.com>
2023-12-27 22:12:53 +0200

    Update environment.ts

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -5,34 +5,32 @@
 export const environment = {
   production: false,
   apiPath: window.location.protocol + '//' + window.location.host + '/',
-  // auth: 
-  emailAndSSNUniqueUrl: 'http://localhost:3001/api/auth/ssn-email-unique/',
-  registerUrl: 'http://localhost:3001/api/auth/register/',
-  loginUrl: 'http://localhost:3001/api/auth/login/',
+  // auth:
+  emailAndSSNUniqueUrl: 'http://62.171.156.199:3001/api/auth/ssn-email-unique/',
+  registerUrl: 'http://62.171.156.199:3001/api/auth/register/',
+  loginUrl: 'http://62.171.156.199:3001/api/auth/login/',
 
-  // categories: 
-  categoriesUrl: 'http://localhost:3001/api/categories/',
+  // categories:
+  categoriesUrl: 'http://62.171.156.199:3001/api/categories/',
 
+  // cate:
+  cateUrl: 'http://62.171.156.199:3001/api/cate/',
 
-  // cate: 
-  cateUrl: 'http://localhost:3001/api/cate/',
+  // products:
+  productsUrl: 'http://62.171.156.199:3001/api/products/',
+  productsImageUrl: 'http://62.171.156.199:3001/shopping/images/',
+  productsCountUrl: 'http://62.171.156.199:3001/api/products-count/',
 
-  // products: 
-  productsUrl: 'http://localhost:3001/api/products/',
-  productsImageUrl: 'http://localhost:3001/shopping/images/',
-  productsCountUrl: 'http://localhost:3001/api/products-count/',
- 
   //carts
-  cartByUserUrl: 'http://localhost:3001/api/cart-by-user/',  //to display when user's cart was createdAt 
+  cartByUserUrl: 'http://62.171.156.199:3001/api/cart-by-user/', //to display when user's cart was createdAt
 
-  // cart items 
-  cartItemsUrl: 'http://localhost:3001/api/items/',
-  cartItemsByCartUrl: 'http://localhost:3001/api/items-by-cart/',  //to display all items in user's current cart
+  // cart items
+  cartItemsUrl: 'http://62.171.156.199:3001/api/items/',
+  cartItemsByCartUrl: 'http://62.171.156.199:3001/api/items-by-cart/', //to display all items in user's current cart
 
-  //  orders: 
-  ordersUrl: 'http://localhost:3001/api/orders/',
-  ordersCountUrl:'http://localhost:3001/api/orders-count/',
-
+  //  orders:
+  ordersUrl: 'http://62.171.156.199:3001/api/orders/',
+  ordersCountUrl: 'http://62.171.156.199:3001/api/orders-count/',
 };
 
 /*
